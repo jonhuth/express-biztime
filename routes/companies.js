eb4cec333b0bffe97a94ba57a7f61c6598b3f034
Jonathan Huth <jonhuth95@gmail.com>
2020-06-18 14:39:26 -0700

    fixed error handling for posting new record

--- routes/companies.js
+++ routes/companies.js
@@ -16,9 +16,11 @@ router.get("/", async function (req, res, next) {
 
 router.get("/:code", async function (req, res, next) {
   try {
-    const company = await db.query(`SELECT code, name, description FROM companies 
-    WHERE code = $1`, [req.params.code])
-    if (!company.rows.length) throw new ExpressError("valid code required", 404);
+    const company = await db.query(
+      `SELECT code, name, description FROM companies 
+        WHERE code = $1`,
+      [req.params.code])
+    if (company.rows.length === 0) throw new ExpressError("valid code required", 404);
 
     return res.json({ company: company.rows[0] })
   } catch (err) {
@@ -29,10 +31,23 @@ router.get("/:code", async function (req, res, next) {
 router.post("/", async function (req, res, next) {
   try {
     const { code, name, description } = req.body;
-    const result = await db.query(`INSERT INTO companies (code, name, description)
-    VALUES ($1, $2, $3)
-    RETURNING code, name, description`,
+
+    const company = await db.query(
+      `SELECT code FROM companies 
+        WHERE code = $1`,
+      [code])
+      //expect company.rows to be empty
+      console.log(company.rows);
+      if (company.rows.length !== 0) throw new ExpressError("record already exists", 400);
+    const result = await db.query(
+      `INSERT INTO companies (code, name, description)
+        VALUES ($1, $2, $3)
+        RETURNING code, name, description`,
       [code, name, description])
+
+
+
+    
 
     return res.json({ company: result.rows[0] });
 
@@ -47,12 +62,15 @@ router.put("/:code", async function (req, res, next) {
     const code = req.params.code;
     const { name, description } = req.body;
 
-    if (Object.keys(req.body).length !== 2) throw new ExpressError("please input name and description", 404);
+    if (Object.keys(req.body).length !== 2) throw new ExpressError("please input name and description", 400);
 
-    const result = await db.query(`UPDATE companies SET name=$1, description=$2
-  WHERE code = $3
-  RETURNING code, name, description`,
+    const result = await db.query(
+      `UPDATE companies SET name=$1, description=$2
+        WHERE code = $3
+        RETURNING code, name, description`,
       [name, description, code])
+
+    if (result.rows.length === 0) throw new ExpressError("Did not update; code does not match valid code", 404);
 
     return res.json({ company: result.rows[0] });
 
@@ -64,10 +82,13 @@ router.put("/:code", async function (req, res, next) {
 router.delete("/:code", async function (req, res, next) {
   try {
 
-    if (Obj.keys(req.body).length === 0) throw new ExpressError("code does not exist", 404);
-    const result = await db.query(`DELETE FROM companies WHERE code = $1`, [req.params.code]);
-    console.log("try to delete bob", result);
-    return;
+    const result = await db.query(
+      `DELETE FROM companies WHERE code = $1
+        RETURNING code`,
+      [req.params.code]);
+    if (result.rows.length === 0) throw new ExpressError("code does not exist", 404);
+
+    return res.json({ status: "deleted" });
 
   } catch (err) {
     next(err);
